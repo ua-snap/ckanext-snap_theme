267ae3e40c304580773d3efd822cc340bc4a4330
Bruce Crevensten <bruce.crevensten@gmail.com>
2015-01-31 03:27:51 +0000

    adds resolution/extent, hides additional information by default

--- ckanext/snap_theme/plugin.py
+++ ckanext/snap_theme/plugin.py
@@ -2,6 +2,7 @@ import ast
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckan.lib.helpers as helpers
+from datetime import datetime
 
 class SnapThemePlugin(plugins.SingletonPlugin):
     # Declare that this class implements IConfigurer.
@@ -27,5 +28,30 @@ class SnapThemePlugin(plugins.SingletonPlugin):
         Adds any additional data fields to the package dictionary for custom display
         '''
         # Turn the stored string of credits back into a list 
-        pkg_dict['credits'] = ast.literal_eval(helpers.get_pkg_dict_extra(pkg_dict, 'credits', ''))
+        credits = helpers.get_pkg_dict_extra(pkg_dict, 'credits', '');
+        if credits:
+            pkg_dict['credits'] = ast.literal_eval(credits)
+
+        # If temporal extent, format the dates.
+        temporal_start = helpers.get_pkg_dict_extra(pkg_dict, 'temporal-extent-begin')
+        temporal_end = helpers.get_pkg_dict_extra(pkg_dict, 'temporal-extent-end')
+        if temporal_start and temporal_end:
+            # Unfortunately, the datetime library won't handle years before 1900, so a bit
+            # of manual parsing is needed here.
+            start_year = temporal_start[:4]
+            start_month = temporal_start[5:7]
+            end_year = temporal_end[:4]
+            end_month = temporal_end[5:7]
+            try:
+                pkg_dict['temporal_start'] = "{0} {1}".format(datetime.strptime(start_month, '%m').strftime('%B'), start_year)
+                pkg_dict['temporal_end'] = "{0} {1}".format(datetime.strptime(end_month, '%m').strftime('%B'), end_year)
+            except ValueError:
+                # Swallow and ignore if the date parsing failed.
+                pass
+        
+        spatial_resolution = helpers.get_pkg_dict_extra(pkg_dict, 'spatial-resolution')
+        spatial_resolution_units = helpers.get_pkg_dict_extra(pkg_dict, 'spatial-resolution-units')
+        if spatial_resolution and spatial_resolution_units:
+            pkg_dict['spatial_resolution'] = "{0} {1}".format(spatial_resolution, spatial_resolution_units)
+
         return pkg_dict
